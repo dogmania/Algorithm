ffe09a7ca39d8954a3396d4c1d1de19b9c645567
Hyeonsu Son <107483302+dogmania@users.noreply.github.com>
2024-01-27 18:52:08 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = 0;
+    
+    for (int i = 0; i <= 9; i++) {
+        if (find(numbers.begin(), numbers.end(), i) == numbers.end()) answer += i;
+    }
+    
+    return answer;
+}
