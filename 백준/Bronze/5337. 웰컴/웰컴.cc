720544d646c7fa5bd18b4faec4a4762dbb9a1d6a
Hyeonsu Son <107483302+dogmania@users.noreply.github.com>
2024-03-17 22:54:57 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <iterator>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cout << ".  .   ." << '\n';
+    cout << "|  | _ | _. _ ._ _  _" << '\n';
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+}
