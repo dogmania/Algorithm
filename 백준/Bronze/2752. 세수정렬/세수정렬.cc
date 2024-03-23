a023c9fda2f7dc0a40a26102f51cb33f9e7d6819
Hyeonsu Son <107483302+dogmania@users.noreply.github.com>
2024-03-23 23:38:52 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> num(3, 0);
+    
+    cin >> num[0] >> num[1] >> num[2];
+    
+    sort(num.begin(), num.end());
+    
+    cout << num[0] << ' ' << num[1] << ' ' << num[2] << '\n';
+}
