2d4273f5f0608b21d32596c7eb896c3849c87f6c
Hyeonsu Son <107483302+dogmania@users.noreply.github.com>
2024-06-22 23:20:54 +0900

    [Silver V] Title: 스네이크버드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <iterator>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n = 0;
+    int ans = 0;
+    int index = 0;
+
+    cin >> n >> ans;
+
+    vector<int> v(n, 0);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; i++) {
+        if (v[i] <= ans) ans++;
+        else break;
+    }
+
+    cout << ans << '\n';
+}
