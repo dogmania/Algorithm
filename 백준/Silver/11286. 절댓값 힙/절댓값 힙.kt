d025e977e315969614daddefcb391400e6c49496
Hyeonsu Son <107483302+dogmania@users.noreply.github.com>
2024-10-28 22:44:43 +0900

    [Silver I] Title: 절댓값 힙, Time: 752 ms, Memory: 44452 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.kt
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue
+import kotlin.math.abs
+
+fun main() {
+val n = readln().toInt()
+val queue = PriorityQueue<Int> { a, b ->
+    if (abs(a) == abs(b)) a - b else abs(a) - abs(b)
+}
+
+repeat(n) {
+    val num = readln().toInt()
+
+    if (num == 0) {
+        if (queue.isEmpty()) {
+            println(0)
+        } else {
+            println(queue.poll())
+        }
+    } else {
+        queue.add(num)
+    }
+}
+}
