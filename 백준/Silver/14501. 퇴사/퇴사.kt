e24f1cc13ddf26e1ac7ee41c8816cb77be163884
Hyeonsu Son <107483302+dogmania@users.noreply.github.com>
2024-10-29 13:34:49 +0900

    [Silver III] Title: 퇴사, Time: 160 ms, Memory: 26488 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.kt
@@ -0,0 +1,15 @@
+fun main() {
+val n = readln().toInt()
+
+val arr = Array(n) { readln().split(" ").map{ it.toInt() } }
+val dp = Array(n + 1) { 0 }
+
+for (i in 0 until n) {
+    if (i + arr[i][0] <= n) {
+        if (i > 0) dp[i] = maxOf(dp[i], dp[i - 1])
+        dp[i + arr[i][0]] = maxOf(dp[i + arr[i][0]], dp[i] + arr[i][1])
+    }
+}
+
+println(dp.maxOrNull()!!)
+}
