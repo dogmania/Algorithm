8c030ef5adfa67fa884e14bc46c5e8967e3bc077
Hyeonsu Son <107483302+dogmania@users.noreply.github.com>
2025-10-09 18:06:19 +0900

    [Gold IV] Title: 물통, Time: 216 ms, Memory: 33820 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.kt
+++ 백준/Gold/2251. 물통/물통.kt
@@ -1,87 +1,73 @@
 import java.util.*
 import java.io.*
-import kotlin.math.max
-
-data class Bottle(
-    val a: Int,
-    val b: Int,
-    val c: Int 
-)
-
-fun bfs(
-    start: Bottle, 
-    visited: MutableList<MutableList<MutableList<Boolean>>>, 
-    answer: MutableList<Int>,
-    A: Int, 
-    B: Int,
-    C: Int
-) {
-    val dq = ArrayDeque<Bottle>()
-    
-    dq.addLast(start)
-    visited[start.a][start.b][start.c] = true
-
-    fun tryAdd(newA: Int, newB: Int, newC: Int) {
-        if (!visited[newA][newB][newC]) {
-            visited[newA][newB][newC] = true
-            dq.addLast(Bottle(newA, newB, newC))
-        }
-    }
-
-    while(dq.isNotEmpty()) {
-        val (a, b, c) = dq.removeFirst()
-        
-        if (a == 0) answer.add(c)
-
-        if (a + b <= B) {
-            tryAdd(0, a + b, c)
-        } else {
-            tryAdd(a + b - B, B, c)
-        }
-        
-        if (a + c <= C) {
-            tryAdd(0, b, a + c)
-        } else {
-            tryAdd(a + c - C, b, C)
-        }
-        
-        if (a + b <= A) {
-            tryAdd(a + b, 0, c)
-        } else {
-            tryAdd(A, a + b - A, c)
-        }
-        
-        if (b + c <= C) {
-            tryAdd(a, 0, b + c)
-        } else {
-            tryAdd(a, b + c - C, C)
-        }
-        
-        if (a + c <= A) {
-            tryAdd(a + c, b, 0)
-        } else {
-            tryAdd(A, b, a + c - A)
-        }
-        
-        if (b + c <= B) {
-            tryAdd(a, b + c, 0)
-        } else {
-            tryAdd(a, B, b + c - B)
-        }
-    }
-}
+import kotlin.math.*
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
     val (A, B, C) = br.readLine().split(" ").map{ it.toInt() }
-    val visited = MutableList(201) { MutableList(201) { MutableList(201) { false } } }
-    val answer = mutableListOf<Int>()
+    val visited = Array(201) { Array(201) { BooleanArray(201) { false } } }
+    val answer = mutableSetOf<Int>()
 
-    bfs(Bottle(0, 0, C), visited, answer, A, B, C)
+    fun bfs() {
+        val dq = ArrayDeque<Triple<Int, Int, Int>>()
 
-    answer.sortWith(compareBy<Int> { it })
-    bw.write(answer.joinToString(" "))
+        dq.addLast(Triple(0, 0, C))
+        visited[0][0][C] = true
+
+        fun tryAdd(newA: Int, newB: Int, newC: Int) {
+            if (!visited[newA][newB][newC]) {
+                dq.addLast(Triple(newA, newB, newC))
+                visited[newA][newB][newC] = true
+            }
+        }
+
+        while(dq.isNotEmpty()) {
+            val (a, b, c) = dq.removeFirst()
+
+            if (a == 0) answer.add(c)
+
+            if (a + b <= B) {
+                tryAdd(0, a + b, c)
+            } else {
+                tryAdd(a + b - B, B, c)
+            }
+
+            if (a + c <= C) {
+                tryAdd(0, b, a + c)
+            } else {
+                tryAdd(a + c - C, b, C)
+            }
+
+            if (b + a <= A) {
+                tryAdd(b + a, 0, c)
+            } else {
+                tryAdd(A, b + a - A, c)
+            }
+
+            if (b + c <= C) {
+                tryAdd(a, 0, b + c)
+            } else {
+                tryAdd(a, b + c - C, C)
+            }
+
+            if (c + a <= A) {
+                tryAdd(c + a, b, 0)
+            } else {
+                tryAdd(A, b, c + a - A)
+            }
+
+            if (c + b <= B) {
+                tryAdd(a, c + b, 0)
+            } else {
+                tryAdd(a, B, c + b - B)
+            }
+        }
+    }
+
+    bfs()
+
+    bw.write(answer.toMutableList().sorted().joinToString(" ") + "\n")
 
     bw.flush()
     bw.close()
